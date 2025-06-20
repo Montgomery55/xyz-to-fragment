42e261533810344e680250be12dd65d30a38e50a
Montgomery Gray <montgomerygray55@gmail.com>
2025-06-20 13:55:37 -0400

    add readme update main.py

---
+++ README.md
@@ -0,0 +1,15 @@
+# Fragmentation Library
+
+This is a library for obtaining noncovalent fragments of xyz files capable of producing tinker input files, ghost atom fragmentation, and general fragment xyz files.
+
+## Usage
+
+talk about what main does and what the functions do
+
+## Installation
+
+talk about needed python libraries and why required files are here
+
+## Tests
+
+how to run tests
--- main.py
+++ main.py
@@ -7,12 +7,15 @@ sample = XYZ(file)
 sample.reader()
 sample.bond_order_connectivities()
 
+#sample tinker input generator
 sample.tinker_input_generator('modules/basic_ff_atom_types.txt')
 print()
+#sample fragmentation
 sample_frag = sample.fragment()
 for frag in sample_frag:
     for atom in frag:
         print(atom[0], atom[1], atom[2], atom[3])
     print()
 print()
+#sample ghost atom generator
 sample.ghost_atom_generator()
