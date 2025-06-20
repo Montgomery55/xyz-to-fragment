3f80757f3d67db7a401288577b84d3db3777c963
Montgomery Gray <montgomerygray55@gmail.com>
2025-06-20 13:45:33 -0400

    main includes sample calcs

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from modules.xyz_reader import *
 from modules.bond_distances import *
 
-file = 'tests/MeNH2_peptide.xyz'
+file = 'tests/water_cluster.xyz'
 
-test = XYZ(file)
-test.reader()
-test.bond_order_connectivities()
+sample = XYZ(file)
+sample.reader()
+sample.bond_order_connectivities()
 
-test.ghost_atom_generator()
+sample.tinker_input_generator('modules/basic_ff_atom_types.txt')
+print()
+sample_frag = sample.fragment()
+for frag in sample_frag:
+    for atom in frag:
+        print(atom[0], atom[1], atom[2], atom[3])
+    print()
+print()
+sample.ghost_atom_generator()
